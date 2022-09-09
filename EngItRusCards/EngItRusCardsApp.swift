02903eaf08a2c2281d9fa55bb77d6d484dfc570a
Greenpiligrim <71882770+Greenpiligrim@users.noreply.github.com>
2022-09-09 22:53:35 +0200

    Initial Commit

---
+++ EngItRusCards/EngItRusCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EngItRusCardsApp.swift
+//  EngItRusCards
+//
+//  Created by Vladimir Vasilenko on 09/09/22.
+//
+
+import SwiftUI
+
+@main
+struct EngItRusCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
