09ba03ed83a08131de5b1071bb5b58a292df2769
Greenpiligrim <71882770+Greenpiligrim@users.noreply.github.com>
2022-09-20 16:25:28 +0200

    almost done

--- EngItRusCards/Views/TestView.swift
+++ EngItRusCards/Views/TestView.swift
@@ -9,16 +9,24 @@ import SwiftUI
 import Foundation
 
 struct TestView: View {
-    let people = EnglishFrasesDataServise.FrasesCard
+    let dataCard = EnglishFrasesDataServise.FrasesCard
+    @State var italian: Bool = false
+    @State var wiseWersa: Bool = false
 
     var body: some View {
         ZStack {
-            ForEach(people) { p in
-                CardView(cardData: p)
+            Color.orange.ignoresSafeArea()
+            ForEach(dataCard) { cardData in
+                CardView(cardData: cardData, wiseWersa: $wiseWersa, italian: $italian)
             }
         }
     }
 }
+
+
+
+
+
 
 struct TestView_Previews: PreviewProvider {
     static var previews: some View {
@@ -27,46 +35,49 @@ struct TestView_Previews: PreviewProvider {
 }
 
 
+
+
+
 struct CardView: View {
-    var cardData: Frases
+    let cardData: Frases
     @State private var offset = CGSize.zero
     @State private var color: Color = .black
     
-    
+    @Binding var wiseWersa: Bool//*
+    @Binding var italian: Bool//*
+    @State var showTranslate: Bool = false
+
     
     
     var body: some View {
-        ZStack {
-            Rectangle()
+
+                VStack {
+                    ArrowFlag(wiseWersa: $wiseWersa, italian: $italian)
+                    bigText
+                    littleText
+                
+                }
                 .frame(width: 300, height: 400)
+                .background()
                 .cornerRadius(30)
-                .shadow(radius: 5)
-            
-            
-            HStack {
-                Text(cardData.it)
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-           
-            }
-        }
-        .offset(x: offset.width, y: offset.height * 0.4)
-        .rotationEffect(.degrees(Double(offset.width / 40)))
-        .gesture(
-        DragGesture()
-            .onChanged { gesture in
-                offset = gesture.translation
-                withAnimation {
-                    changeColor(width: offset.width)
-                }
-            }
-            .onEnded { _ in
-                withAnimation {
-                    swipeCard(width: offset.width)
-                    changeColor(width: offset.width)
-                }
-            }
-        )
+                .shadow(color: .red, radius: offset != .zero ? 5 : 0.5)
+                .offset(x: offset.width, y: offset.height * 0.4)
+                .rotationEffect(.degrees(Double(offset.width / 40)))
+                .gesture(
+                    DragGesture()
+                        .onChanged { gesture in
+                            offset = gesture.translation
+                            withAnimation {
+                                changeColor(width: offset.width)
+                            }
+                        }
+                        .onEnded { _ in
+                            withAnimation {
+                                swipeCard(width: offset.width)
+                                changeColor(width: offset.width)
+                            }
+                        }
+                )
     }
     
     func swipeCard(width: CGFloat) {
@@ -92,5 +103,77 @@ struct CardView: View {
             color = .black
         }
     }
+    
+}
 
+
+extension CardView {
+    private var bigText: some View {
+        VStack() {
+            Text(italian ? (wiseWersa ? cardData.rus : cardData.eng) : (wiseWersa ? cardData.rusIt : cardData.it))
+                .font(.largeTitle)
+                .bold()
+                .multilineTextAlignment(.center)
+                .padding()
+        }.padding()
+    }
+    
+    private var littleText: some View {
+        HStack {
+            Text(italian ? (showTranslate ? "*" + (wiseWersa ? cardData.eng.uppercased() : cardData.rus.uppercased()) : (wiseWersa ? "* подсказка" : "* tap to show translate" )) : ((showTranslate ? "*" + (wiseWersa ? cardData.it.uppercased() : cardData.rusIt.uppercased()) : (wiseWersa ? "* подсказка" : "* toccare per visualizzare la traduzione" ))))
+                .font(.subheadline)
+                .fontWeight(.thin)
+                .multilineTextAlignment(.center)
+                .transition(.slide)
+        }
+        .padding()
+        .onTapGesture {
+            withAnimation(.easeOut) {
+                showTranslate.toggle()
+            }
+        }
+    }
+}
+
+struct ArrowFlag: View {
+    @Binding var wiseWersa: Bool
+    @Binding var italian: Bool
+    
+    var body: some View {
+        VStack {
+            HStack(spacing: 160) {
+                Text("🇬🇧")
+                    .scaleEffect(italian ? 1.6 : 0.8)
+                    .onTapGesture {
+                        withAnimation {
+                            italian.toggle()
+                        }
+                    }
+                    .shadow(radius: italian ? 7.0 : 1.0)
+
+
+                Text("🇮🇹")
+                    .scaleEffect(italian ? 0.8 : 1.6)
+                    .onTapGesture {
+                        withAnimation(.default) {
+                            italian.toggle()
+                        }
+                    }
+                    .shadow(radius: italian ? 1.0 : 7.0)
+            }.padding()
+
+            Button(action: {
+                withAnimation {
+                    wiseWersa.toggle()
+                }
+            }, label: {
+                Image(systemName: "arrow.2.circlepath")
+                    .font(.title3)
+                    .tint(.black)
+                    .rotationEffect(Angle(degrees: wiseWersa ? 180 : 0))
+            })
+
+        }
+        .frame(maxWidth: .infinity)
+    }
 }
